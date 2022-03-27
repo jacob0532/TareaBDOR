2f5e38dde2088cf1b100639fb372dc62652831f5
Blacksolid64 <48729275+Blacksolid64@users.noreply.github.com>
2022-03-26 19:08:19 -0600

    Inserts v1

---
+++ BDOR_Inserts.sql
@@ -0,0 +1,24 @@
+---------------------------------Clientes--------------------------------------------
+SELECT public.clientes_create('Mario','Mcgregor','Mena',653045898,33,'Tarjeta BAC');
+SELECT public.clientes_create('Jaquith', 'Dyball', 'Bortolini', 8621863,85, 'Cheque');
+SELECT public.clientes_create('Orazio', 'Veivers', 'Papen', 3617652, 65, 'Efectivo');
+SELECT public.clientes_create('Ernie', 'Allbon', 'Heintzsch', 9950444, 70, 'Tarjeta');
+SELECT public.clientes_create('Dante', 'Guislin', 'Largan', 2036026, 82, 'Cheque');
+SELECT public.clientes_create('Wileen', 'Kingscott', 'Bradane', 8965996, 93, 'Tarjeta');
+SELECT public.clientes_create('Andreana', 'Whiteman', 'Reimer', 8900460, 38, 'Cheque');
+SELECT public.clientes_create('Andrej', 'Whitehorn', 'Burtwistle', 5318158, 34, 'Tarjeta');
+SELECT public.clientes_create('Norah', 'Scampion', 'Minchell', 4640056, 91, 'Cheque');
+SELECT public.clientes_create('Davita', 'Nairne', 'Heims', 3085375, 26, 'Cheque');
+SELECT public.clientes_create('Diena', 'Widdup', 'Robelin', 3157038, 44, 'Tarjeta');
+-------------------------------------Empleados----------------------------------------
+SELECT public.empleado_create('Rutledge', 'Franz-Schoninger', 'Grieveson', 30, 0133477, 96896386);
+SELECT public.empleado_create('Ari', 'Edmundson', 'Lestrange', 83, 9391031, 73897620);
+SELECT public.empleado_create('Tillie', 'Khilkov', 'Dutson', 12, 5894484, 44738879);
+SELECT public.empleado_create('Shadow', 'Baukham', 'Afield', 74, 0218632, 32767298);
+SELECT public.empleado_create('Ode', 'Dedon', 'Bullcock', 84', 0195784, 21015432);
+SELECT public.empleado_create('Wallace', 'Gauchier', 'Kopmann', 11, 8328032, 04863795);
+SELECT public.empleado_create('Mellisa', 'Shirland', 'Blackstock', 86, 0080887, 76897206);
+SELECT public.empleado_create('Margaretta', 'Mepham', 'Parlett', 73, 9083187, 37290660);
+SELECT public.empleado_create('Iggy', 'Rames', 'Sutheran', 43, 6040411, 46126402);
+SELECT public.empleado_create('Manfred', 'Engledow', 'Kordovani', 55, 7850283, 36861443);
+----------------------------------------Inventario-------------------------------------------
